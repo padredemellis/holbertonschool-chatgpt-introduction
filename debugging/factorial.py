de36c3f73370abf3f9e703867218178c4e541496
padredemellis <10656@holbertonstudents.com>
2025-05-06 13:11:18 +0000

    factorial

---
+++ debugging/factorial.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+if __name__ == "__main__":
+    try:
+        num = int(sys.argv[1])
+        print(factorial(num))
+    except (IndexError, ValueError):
+        print("Usage: ./factorial.py <non-negative-integer>")
